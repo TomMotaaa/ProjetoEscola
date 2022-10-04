758113e6235965a8134c2d8bf4385f3b35cd04fd
EVERTON MOTA DE ASSIS <u21426@LAPA16.academico.cotuca.unicamp.br>
2022-10-04 16:20:24 -0300

    Building

--- src/components/CrudCurso/CrudCurso.js
+++ src/components/CrudCurso/CrudCurso.js
@@ -4,124 +4,129 @@ import { useState, useEffect } from "react";
 import Main from "../template/Main";
 import axios from "axios";
 
-const urlAPI = 'http://localhost:5014/api/curso'
-const title = 'Cadastro de Cursos'
-const initialState = {
-    curso: {id: 0, codCurso: 0, nomeCurso: '', periodo: ''},
-    lista: []
-}
+const Cursos = () => {
+    const title = "Cadastro de Cursos"
+    const urlAPI = 'http://localhost:5014/api/curso';
+    const [data, setData] = useState([])
+    const [lista, setLista] = useState({
+        id: 0,
+        codCurso: '',
+        nomeCurso: '',
+        periodo: '',
+    })
+    const [cursoData, setCursoData] = useState({
+        id: 0,
+        codCurso: '',
+        nomeCurso: '',
+        periodo: '',
+    })
 
-export default function Curso() {
-    const [curso, setCurso] = useState(initialState.curso)
-    const [lista, setLista] = useState(initialState.lista)
-
-    const dadosAPI = async () => {
-        await axios(urlAPI).then((resp) => resp.data).catch((err) => err)
-    }
-
-    useEffect(() => {
-        dadosAPI()
-            .then(setLista)
-    }, [curso])
-
-    const limpar = () => {
-        setCurso({ curso: initialState.curso })
-    }
-
-    const salvar = () => {
-        curso.codCurso = Number(curso.codCurso)
-        const metodo = curso.id ? 'put' : 'post'
-        const url = curso.id ? `${urlAPI}/${curso.id}` : urlAPI
-
-        axios[metodo] (url, curso)
-            .then((resp) => {
-                const lista = getListaAtualizada(resp.data)
-                setCurso({ curso: initialState.curso, lista})
+    const InfoAPI = async () => {
+        await axios(urlAPI)
+            .then(resp => {
+                setData(resp.data)
             })
     }
 
-    const getListaAtualizada = (curso, add = true) => {
-        const listaAtualizada = lista.filter(a => a.id !== curso.id);
-        if(add) listaAtualizada.unshift(curso);
-        return listaAtualizada;
+
+    const atualizaCampo = evento => {
+        const { name, value } = evento.target
+        setCursoData({
+            ...cursoData,
+            [name]: value
+        })
+        console.log(cursoData)
     }
 
-    const atualizaCampo = (evento) => {
-        const {nome, valor} = evento.target
-        setCurso({
-            ...curso,
-            [nome] : valor
+    const getListaAtualizada = (curso, add = true) =>{
+        const listaAtualizada = lista.filter(a => a.id !== curso.id)
+        if(add) listaAtualizada.unshift(curso)
+        return listaAtualizada
+    }
+
+    function salvar () {
+        const dadosCurso = cursoData
+        cursoData.codCurso = Number(dadosCurso.codCurso)
+        const metodo = cursoData.id ? 'put' : 'post'
+        const url = cursoData.id ? `${urlAPI}/${dadosCurso.id}` : urlAPI
+
+        axios[metodo](url, dadosCurso)
+        .then(resp => {
+            let lista = getListaAtualizada(resp.data)
+            cursoData({ dadosCurso: cursoData.dadosCurso, lista})
+            setLista(lista)
         })
     }
 
-    const carregar = (curso) => {
-        setCurso(curso)
-    }
+    const atualizarCurso = (cursoData) => setCursoData(cursoData)
 
-    const remover = (curso) => {
-        const url = urlAPI + "/" + curso.id;
-        if (window.confirm("Confirma remoção do aluno: " + curso.nomeCurso)) {
-            console.log("entrou no confirme da tela")
-        
-        axios['delete'] (url, curso)
-            .then((resp) => {
-                const lista = getListaAtualizada(curso, false)
-                setCurso({ curso: initialState.curso, lista })
+    const removerCurso = (cursoData) => {
+        const url = urlAPI + '/' + cursoData.id;
+        if (!window.confirm("Confirmae remoção do curso: " + cursoData.nomeCurso)) return;
+
+        axios['delete'](url, cursoData)
+            .then((_resp) => {
+                const lista = getListaAtualizada(cursoData, false)
+                setCursoData({ dadosCurso: cursoData.dadosCurso, lista })
             })
     }
 
-    const renderForm = () => {
-        return (
-                <div className="inclui-container">
-                    <label> Código do Curso: </label>
-                    <input
-                        type="number"
-                        id="codCurso"
-                        placeholder="Codigo"
-                        className="form-input"
-                        name="codCurso"
-    
-                    value={curso.codCurso}
-                    onChange={(e) => atualizaCampo(e)}
-                    />
-                    <label> Curso: </label>
-                    <input
-                        type="text"
-                        id="nome"
-                        placeholder="Curso"
-                        className="form-input"
-                        name="nomeCurso"
-    
-                    value={curso.nomeCurso}
-                    onChange={(e) => atualizaCampo(e)}
-                    />
-                    <label> Período: </label>
-                    <input
-                        type="text"
-                        id="codCurso"
-                        placeholder="Periodo"
-                        className="form-input"
-                        name="periodo"
-    
-                    value={curso.periodo}
-                    onChange={(e) => atualizaCampo(e)}
-                    />
-                    <button className="btnSalvar"
-                    onClick={salvar}
-                    >
-                        Salvar
-                    </button>
-                    <button className="btnCancelar"
-                    onClick={limpar} 
-                    >
-                        Cancelar
-                    </button>
-                </div>
-        )    
-    }
 
-    const renderTable = () => {
-        <div className="listagem">
+
+    useEffect(() => {
+        InfoAPI()
+    }, [data])
+
+
+    return (
+        <Main title={title}>
+            <div className="inclui-container">
+                <label> Código do Curso: </label>
+                <input
+                    type="number"
+                    id="ra"
+                    placeholder="Codigo"
+                    className="form-input"
+                    name="codCurso"
+
+                value={cursoData.codCurso}
+                onChange={atualizaCampo}
+                />
+                <label> Curso: </label>
+                <input
+                    type="text"
+                    id="nome"
+                    placeholder="Curso"
+                    className="form-input"
+                    name="nomeCurso"
+
+                value={cursoData.nomeCurso}
+                onChange={atualizaCampo}
+                />
+                <label> Período: </label>
+                <input
+                    type="text"
+                    id="codCurso"
+                    placeholder="Periodo"
+                    className="form-input"
+                    name="periodo"
+
+                value={cursoData.periodo}
+                onChange={atualizaCampo}
+                />
+                <button className="btnSalvar"
+                onClick={salvar}
+                >
+                    Salvar
+                </button>
+                <button className="btnCancelar"
+                //onClick={e => this.limpar(e)} 
+                >
+                    Cancelar
+                </button>
+            </div>
+
+            <div className="listagem">
                 <table className="listaCursos" id="tblListaCursos">
                     <thead className='cabecTabela'>
                         <tr className="cabecTabela">
@@ -132,7 +137,7 @@ export default function Curso() {
                     </thead>
 
                     <tbody>
-                        {lista.map(
+                        {data.map(
                             (curso) =>
                                 <tr key={curso.id}>
                                     <td className="val-center">{curso.nomeCurso}</td>
@@ -140,14 +145,14 @@ export default function Curso() {
                                     <td className="val-center">{curso.periodo}</td>
                                     <td>
                                         <button className='btn-alterar'
-                                        onClick={() => carregar(curso)}
+                                            onClick={() => atualizarCurso(curso)}
                                         >
                                             Alterar
                                         </button>
                                     </td>
                                     <td>
                                         <button className='btn-remover'
-                                        onClick={() => remover(curso)}
+                                            onClick={() => removerCurso(curso)}
                                         >
                                             Remover
                                         </button>
@@ -157,12 +162,8 @@ export default function Curso() {
                     </tbody>
                 </table>
             </div>
-    }
-
-    return (
-        <Main title={title}>
-                {renderForm()}
-                {renderTable()}
         </Main>
     )
-}}
+}
+
+export default Cursos
